6ea2ec85f02a4422b946f1732737c4da8f2ced21
User <User@ADAM>
2017-11-06 19:20:15 +0000

    libs

--- Dream-Girl-Bitch-Fight/Dream-Girl-Bitch-Fight/main.cpp
+++ Dream-Girl-Bitch-Fight/Dream-Girl-Bitch-Fight/main.cpp
@@ -1,3 +1,19 @@
-#include "SDL.h"
+#include <SDL.h>
+#undef main
+
+#ifdef _MSC_VER
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <iostream>
+
+int main(int argc, char*argv[])
+{
+
+
+return 0;
+}
 
 //a
